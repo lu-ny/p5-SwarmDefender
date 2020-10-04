3227ea3fb85823435651cfb38b507c0be4e45155
sgreenlund3 <69866618+sgreenlund3@users.noreply.github.com>
2020-10-03 21:38:13 -0600

    Added fast enemy
    added a blue faster enemy that spawns every 10 to 30 s

--- sketch.js
+++ sketch.js
@@ -1,27 +1,31 @@
 // I originally wanted to create a 'DEFENDER' inspired dogfighting game, 
 // but it turned out very similar to the game 'LUFTRAUSERS'
 // utilizes p5.play
-// next steps: add real sprites/png
+// next steps: dont draw enemies within some r of player
+            //add ship sprites, death/exploding animations
             // add variety of enemies & powerups
 // sugr7650@colorado.edu
 
 var player;
-var enemy;
-var enemyInterval;
-var enemies;
 var bullets;
 var shipImage; 
 var bulletImage;
+
+var enemy;
+var fastBoi;
+var enemyInterval;
+var fastBoiInterval;
+var enemies;
+var enemyBullet;
+var enemyBullets;
+
 var MARGIN = 40;
 var SCENE_W = 4000;
 var SCENE_H = 1660;
 var gravity;
 var gameOver;
-var points;
 var score=0;
-var highscore=0;
-var enemyBullet;
-var enemyBullets;
+var highscore;
 
 //function preload() {
   // load in PNG sprites here instead of drawing squares
@@ -30,7 +34,7 @@ var enemyBullets;
 function setup() {
   c=createCanvas(windowWidth, windowHeight);
   c.position(0, 0);
-
+  highscore = getItem('highscore');
   angleMode(DEGREES);
 
 //create player 
@@ -51,8 +55,8 @@ function setup() {
 //create ground image & sprite
   groundImage = createImage(100,100)
   groundImage.loadPixels();
-    for (let i = 30; i < width - 15; i++) {
-      for (let j = 20; j < height - 25; j++) {
+    for (let i = 0; i < width; i+=1) {
+      for (let j = 0; j < height; j++) {
         let c = color(204 - j, 153 - i, 0);
         set(i, j, c);
       }
@@ -73,12 +77,16 @@ function setup() {
 //create enemy image
   enemyImage = createImage(32,32);
   enemyImage.loadPixels();
+  fastBoiImage = createImage(32,32);
+  fastBoiImage.loadPixels();
     for (let i = 0; i < enemyImage.width; i++) {
       for (let j = 0; j < enemyImage.height; j++) {
         enemyImage.set(i, j, color(100, 50, 92));
+        fastBoiImage.set(i, j, color(50, 50, 255));
       }
     }
   enemyImage.updatePixels();
+  fastBoiImage.updatePixels();
   enemies = new Group();
   
 // set starting condition 
@@ -89,7 +97,7 @@ function setup() {
 function draw() {
   background(50);
   
-// tree placement  
+// ground square thingies placement  
   for (let i=1; i<20; i++){
     rect(map(i,1,20,1,SCENE_W),SCENE_H-50,25,50);
   }
@@ -101,7 +109,11 @@ function draw() {
     fill(255);
     text('Left & Right arrows to rotate; up arrow to accelerate, Z to shoot; Press X to begin', windowWidth/2-45, windowHeight/2+32,150);
     text('Your Score:'+score, windowWidth/2-32, windowHeight/2-32,150)
-    text('Highscore:'+ highscore, windowWidth/2-32, windowHeight/2-64,150)
+    textSize(16)
+    text('Highscore:'+ highscore, windowWidth/2-42, windowHeight/2-64,150)
+    textSize(24)
+    strokeWeight(4)
+    text('Swarm Defender p5', windowWidth/2-96, windowHeight/2-84)
   }
   
   if(gameOver && keyWentDown('x')) {
@@ -164,7 +176,7 @@ function draw() {
       var bullet = createSprite(player.position.x, player.position.y);
       bullet.addImage(bulletImage);
       bullet.setSpeed(10+player.getSpeed(), player.rotation);
-      bullet.life = 30;
+      bullet.life = 50;
       bullet.rotateToDirection = true;
       bullets.add(bullet);
       bulletImage.updatePixels();
@@ -174,10 +186,9 @@ function draw() {
     for (let i=0; i<enemies.length;i++) {
       en = enemies[i];
       en.bounce(enemies)
-      en.maxSpeed = 3;
       en.life = 3;
-      if (abs(en.position.x-player.position.x)<1000) {
-        en.attractionPoint(0.08, player.position.x+random(10,100), player.position.y+random(10,100))
+      if (abs(en.position.x-player.position.x)>1000) {
+        en.attractionPoint(0.1, player.position.x+random(10,100), player.position.y+random(10,100))
       }
       else {
         en.attractionPoint(0.5, player.position.x+random(10,100), player.position.y+random(10,100))
@@ -203,10 +214,22 @@ function drawEnemy() {
     enemy = createSprite(ax,ay);
     enemy.setCollider('circle', 0, 0, 28);
     enemy.addImage(enemyImage);
-    enemy.attractionPoint(2, player.position.x, player.position.y);
     enemy.rotateToDirection=true;
-    enemy.maxSpeed = 6;
+    enemy.maxSpeed = 3;
     enemies.add(enemy); 
+  } 
+}
+
+function drawfastBoi() {
+  if (enemies.length<=50) {
+    var ax = random(50, SCENE_W-50);
+    var ay = random(50, SCENE_H-50);
+    fastBoi = createSprite(ax,ay);
+    fastBoi.setCollider('circle', 0, 0, 28);
+    fastBoi.addImage(fastBoiImage);
+    fastBoi.rotateToDirection=true;
+    fastBoi.maxSpeed = 8;
+    enemies.add(fastBoi); 
   } 
 }
 
@@ -235,7 +258,12 @@ function enemyShoot() {
 function enemyHit(enemy, bullet) {
   enemy.remove();
   bullet.remove();
-  score++
+  if (enemy==fastBoi) {
+    score+=5; //need to fix this, still only adding 1
+  }
+  else {
+    score++
+  }
   if (score>highscore) {
     highscore = score;
   }
@@ -251,8 +279,10 @@ function die() {
   player.velocity.x=0;
   player.velocity.y=0;
   clearInterval(enemyInterval);
-  clearInterval(shootingInterval)
+  clearInterval(shootingInterval);
+  clearInterval(fastBoiInterval);
   //camera.off();
+  storeItem('highscore', highscore)
 }
 
 function newGame() {
@@ -266,6 +296,7 @@ function newGame() {
   player.rotation=0;
   gravity=0.3;
   enemyInterval = setInterval(drawEnemy, 1000); //call function to draw enemy every 1s
-  shootingInterval = setInterval(enemyShoot, 1000); //call function to draw enemy every 1s
+  shootingInterval = setInterval(enemyShoot, 1000); //call function for enemy to shoot every 1s
+  fastBoiInterval = setInterval(drawfastBoi, random(10000, 30000)); //call function to draw fast enemy between 10 and 30 seconds
   score=0;
 }
